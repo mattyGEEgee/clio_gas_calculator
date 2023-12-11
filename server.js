3606caa1ed546e1bad5413134db10643b3f3c2bc
MattyGEEgee <mattgibbs@protonmail.com>
2023-12-11 16:49:42 +0100

    Added all Dev Dependencies
    Expressjs
    nodemon

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const app = express()
 const portNumber = 5001
+
+app.get('/', (req, res) => {
+    res.status(200).send("<h1>THIS is running in docker</h1>")
+})
+
+app.listen(portNumber, () => {
+    console.log("Listening on docker port " + portNumber + "...");
+})
